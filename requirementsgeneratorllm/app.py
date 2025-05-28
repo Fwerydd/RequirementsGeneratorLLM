d8f27db1cf3029ab7f7dd9d0dddc84d2f48904cc
fr-froussel <4551879+fr-froussel@users.noreply.github.com>
2025-05-28 16:49:38 +0200

    UX improvement

--- requirementsgeneratorllm/app.py
+++ requirementsgeneratorllm/app.py
@@ -20,14 +20,28 @@ st.markdown(
 This tool helps you generate technical requirements based on [IREB](https://www.ireb.org/en/) best practices, using LLMs.
 """
 )
+st.markdown("---")
 
 # Project Description
-project_description = st.text_area("Describe your software:", height=200)
+project_description = st.text_area(
+    label="Describe your software:",
+    height=200,
+    help="Describe your software's expectations precisely",
+)
+
+# Requirements Language
+selected_language = st.selectbox(
+    label="Select the requirements language:",
+    options=sorted(LANGUAGES),
+    help="Choose the language of the requirements.",
+)
+
 
 # Requirement Types
 selected_requirements_types = st.multiselect(
-    "Select the requirement types you want to generate:",
-    sorted(REQUIREMENT_TYPES.keys()),
+    label="Select the requirement types you want to generate:",
+    options=sorted(REQUIREMENT_TYPES.keys()),
+    help="Choose the categories of requirements to be generated.",
 )
 # Requirement Type Descriptions
 with st.expander(
@@ -39,49 +53,48 @@ with st.expander(
             unsafe_allow_html=True,
         )
 
-# Requirements Language
-selected_language = st.selectbox("Select the requirements language:", sorted(LANGUAGES))
-
 # Deployment Countries
 selected_countries = st.multiselect(
-    "Select countries where the application will be deployed (optional):",
-    sorted(COUNTRIES),
+    label="Select countries where the application will be deployed (optional):",
+    options=sorted(COUNTRIES),
+    help="Choose the countries where the software will be deployed.",
 )
 
+st.markdown("---")
 
 # Submit Button
+disable_button = not (
+    project_description and selected_requirements_types and selected_language
+)
 left, middle, right = st.columns(3)
-if middle.button("Generate Requirements", type="primary"):
-    if (
-        not project_description
-        or not selected_requirements_types
-        or not selected_language
-    ):
-        st.error("Please complete all fields before generating requirements.")
-    else:
-        with st.spinner("Generating requirements using LLM..."):
-            prompts = build_prompts(
-                project_description=project_description,
-                requirement_types=selected_requirements_types,
-                selected_countries=selected_countries,
-                selected_language=selected_language,
-            )
-            answers = execute_prompts(prompts=prompts)
+if middle.button("Generate Requirements", type="primary", disabled=disable_button):
+    with st.spinner("Generating requirements using Mistral LLM..."):
+        prompts = build_prompts(
+            project_description=project_description,
+            requirement_types=selected_requirements_types,
+            selected_countries=selected_countries,
+            selected_language=selected_language,
+        )
+        answers = execute_prompts(prompts=prompts)
 
-            PROJECT_SETTINGS = f"""
-                Project description: {project_description},
-                Requirement types: {selected_requirements_types},
-                Selected countries: {selected_countries},
-                Selected language: {selected_language}  
-            """
+        PROJECT_SETTINGS = f"""
+            Project description: {project_description},
+            Requirement types: {selected_requirements_types},
+            Selected countries: {selected_countries},
+            Selected language: {selected_language}  
+        """
 
-            FILEPATH = save_requirements(
-                project_settings=PROJECT_SETTINGS, requirements_text=answers
-            )
+        FILEPATH = save_requirements(
+            project_settings=PROJECT_SETTINGS, requirements_text=answers
+        )
 
-            st.success("Prompt generated and requirements saved.")
-            st.download_button(
-                "Download Requirements",
-                data="\n".join(answers),
-                file_name=FILEPATH,
-            )
+    st.success("Requirements generated successfully!")
+    st.download_button(
+        "Download Requirements", data="\n".join(answers), file_name="requirements.txt"
+    )
+    if answers and len(answers[0].splitlines()) > 10:
+        st.markdown("### Preview of Generated Requirements:")
+        st.code(
+            "\n".join([line for line in answers[0].splitlines()[:10] if line]),
+            language="text",
+        )
